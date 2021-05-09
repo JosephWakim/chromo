fix simple controller and end-pivot axis of rotation

chromo/mc/__init__.py
@@ -127,7 +127,12 @@ def _polymer_in_field(
         (default = '.')
     """
     if mc_move_controllers is None:
-        mc_move_controllers = all_moves(output_dir, SimpleControl)
+        mc_move_controllers = all_moves(
+            log_dir=output_dir, 
+            polymers=polymers,
+            move_amp_bounds=(0.05,np.pi),
+            controller=SimpleControl
+        )
 
     for mc_count in range(num_saves):
         mc_sim(polymers, marks, num_save_mc, mc_move_controllers, field)

chromo/mc/mc_controller.py
@@ -3,12 +3,13 @@
 
 # Built-in Modules
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 # External Modules
 import numpy as np
 
 # Custom modules
+from chromo.components import Polymer
 import chromo.mc.mc_stat as mc_stat
 import chromo.mc.moves as mv
 
@@ -19,21 +20,42 @@ class Controller(ABC):
 
     def __init__(
         self,
-        mc_adapter: mv.MCAdapter
+        mc_adapter: mv.MCAdapter,
+        bead_amp_bounds: Tuple[int, int],
+        move_amp_bounds: Tuple[float, float]
     ):
         """Initialize the MC controller.
 
         Parameters
         ----------
-        mc_adapter: mv.MCAdapter
+        mc_adapter : mv.MCAdapter
+            Monte Carlo move adapter affected by controller
+        bead_amp_bounds : Tuple[int, int]
+            Bounds of bead selection amplitude permitted by controller in the
+            form (minimum bead amplitude, maximum bead amplitude)
+        move_amp_bounds : Tuple[float, float]
+            Bounds for move amplitudes permitted by controller in the form
+            (minimum move amplitude, maximum move amplitude)
         """
+        if bead_amp_bounds[0] > bead_amp_bounds[1]:
+            raise ValueError(
+                "Lower bead amplitude bound must be less than \
+                upper bead amplitude bound"
+            )
+        if move_amp_bounds[0] > move_amp_bounds[1]:
+            raise ValueError(
+                "Lower move amplitude bound must be less than \
+                upper move amplitude bound"
+            )
         self.move = mc_adapter
-        
+        self.bead_amp_bounds = bead_amp_bounds
+        self.move_amp_bounds = move_amp_bounds
+
+    @abstractmethod    
     def update_amplitudes(self):
         """Update bead selection and move amplitudes.
         """
-        self.update_bead_amplitude()
-        self.update_move_amplitude()
+        pass
 
     @abstractmethod
     def update_move_amplitude(self):
@@ -57,6 +79,10 @@ class Controller(ABC):
 class NoControl(Controller):
     """Class representation of a trivial, non-acting MC controller.
     """
+    def update_amplitudes(self):
+        """Trivially maintain current move and bead amplitudes.
+        """
+        return
 
     def update_move_amplitude(self):
         """Trivially maintain current move amplitude.
@@ -72,55 +98,108 @@ class NoControl(Controller):
 class SimpleControl(Controller):
     """Apply factor changes to move or bead amplitude based on acceptance rate.
 
-    Based on threshold cutoffs in move acceptance rate, a factor change is
-    applied to the move or bead amplitudes. The change drives the acceptance
-    rate towards a setpoint value of 0.5.
-
-    Changes to the move and bead amplitudes are made based on the following
-    acceptance rate thresholds:
-
-    0 to 0.3        Multiply move amplitude by a factor between 0 and 1
-    0.3 to 0.45     Multiply bead amplitude by a factor between 0 and 1
-    0.45 to 0.55    No change to move or bead acceptance rate
-    0.55 to 0.7     Divide bead amplitude by a factor between 0 and 1
-    0.7 to 1        Divide move amplitude by a factor between 0 and 1
+    A factor change is applied to the move amplitude based on the acceptance
+    rate – if the acceptance rate is less than a setpoint (default 0.5), move
+    amplitude is increased; otherwise, the move amplitude is decreased. Once 
+    the move amplitude reaches an upper or lower threshold, rather than 
+    changing the move amplitude, the bead amplitude is increased or decreased
+    by a certain length, and the move amplitude is reset to its lowest value.
     """
 
-    def update_move_amplitude(self, factor: Optional[float] = 0.95):
-        """Update the move amplitude based on the acceptance rate.
+    def update_amplitudes(
+        self,
+        setpoint_acceptance: Optional[float] = 0.5,
+        move_adjust_factor: Optional[float] = 0.95,
+        num_delta_beads: Optional[int] = 1
+    ):
+        """ Update move and bead amplitudes using a simple controller.
 
         Parameters
         ----------
-        factor : Optional[float]
-            Factor by which to multiply or divide move/bead amplitudes in
-            response to current acceptance rate (default = 0.95)
+        setpoint_acceptance : Optional[float]
+            Optional acceptance rate setpoint (default = 0.5)
+        move_adjust_factor : Optional[float]
+            Factor by which to multiply or divide move amplitude in response
+            to current acceptance rate, between 0 and 1 (default = 0.95)
+        num_delta_beads : Optional[int]
+            Number of beads by which to adjust bead amplitude (default = 1)
+        """
+        self.update_move_amplitude(
+            setpoint_acceptance, move_adjust_factor, num_delta_beads
+        )
+
+    def update_move_amplitude(
+        self,
+        setpoint_acceptance: Optional[float] = 0.5,
+        move_adjust_factor: Optional[float] = 0.95,
+        num_delta_beads: Optional[int] = 1
+    ):
+        """ Update move amplitude based on acceptance rate.
+
+        Parameters
+        ----------
+        setpoint_acceptance : Optional[float]
+            Optional acceptance rate setpoint (default = 0.5)
+        move_adjust_factor : Optional[float]
+            Factor by which to multiply or divide move amplitude in response
+            to current acceptance rate, between 0 and 1 (default = 0.95)
+        num_delta_beads : Optional[int]
+            Number of beads by which to adjust bead amplitude (default = 1)
         """
         acceptance = self.move.performance_tracker.acceptance_rate
         
-        if acceptance >= 0 and acceptance <= 0.3:
-            self.move.amp_move *= factor
-        elif acceptance >= 0.7 and acceptance <= 1:
-            self.move.amp_move /= factor
+        if acceptance < setpoint_acceptance:
+            prop_move_amp = self.move.amp_move * move_adjust_factor
+            if prop_move_amp > self.move_amp_bounds[0]:
+                self.move.amp_move = prop_move_amp
+            else:
+                self.move.amp_bead = max(
+                    self.bead_amp_bounds[0],
+                    self.update_bead_amplitude(
+                        increase=False, num_delta_beads=num_delta_beads
+                    )
+                )
+        elif acceptance > setpoint_acceptance:
+            prop_move_amp = self.move.amp_move / move_adjust_factor
+            if prop_move_amp < self.move_amp_bounds[1]:
+                self.move.amp_move = prop_move_amp
+            else:
+                self.move.amp_bead = min(
+                    self.bead_amp_bounds[1],
+                    self.update_bead_amplitude(
+                        increase=True, num_delta_beads=num_delta_beads
+                    )
+                )
 
-    def update_bead_amplitude(self, factor: Optional[float] = 0.95):
+    def update_bead_amplitude(
+        self, increase: bool, num_delta_beads: Optional[int] = 1
+    ):
         """Update the bead amplitude based on the acceptance rate.
 
         Parameters
         ----------
-        factor : Optional[float]
-            Factor by which to multiply or divide move/bead amplitudes in
-            response to current acceptance rate (default = 0.95)
-        """
-        acceptance = self.move.performance_tracker.acceptance_rate
+        increase : bool
+            True or false indicator for whether to increase bead amplitude
+        num_delta_bead: Optional[int]
+            Number of beads by which to increase bead amplitude (default = 1)
 
-        if acceptance > 0.3 and acceptance <= 0.45:
-            self.move.amp_bead *= factor
-        elif acceptance >= 0.55 and acceptance < 0.7:
-            self.move.amp_bead /= factor
+        Returns
+        -------
+        prop_bead_amp : int
+            Proposed bead amplitude after adjustment
+        """
+        if increase:
+            self.move.amp_move = self.move_amp_bounds[0]
+            return self.move.amp_bead + num_delta_beads
+        else:
+            self.move.amp_move = self.move_amp_bounds[1]
+            return self.move.amp_bead - num_delta_beads
 
 
 def all_moves(
     log_dir: str,
+    polymers: List[Polymer],
+    move_amp_bounds: Tuple[float, float],
     controller: Optional[Controller] = NoControl
 ) -> List[Controller]:
     """Generate a list of controllers for all adaptable MC moves.
@@ -129,6 +208,11 @@ def all_moves(
     ----------
     log_dir : str
         Path to the directory in which to save log files
+    polymers : List[Polymer]
+        Polymers on which the move is applied
+    max_move_amp : Tuple[float, float]
+        Bounds to which move amplitudes may be adjusted, in the form (lower
+        move amplitude bound, upper move amplitude bound)
     controller : Optional[Controller]
         Bead and move amplitude controller (default = NoControl)
     
@@ -136,9 +220,14 @@ def all_moves(
     -------
     List of controllers for all adaptable MC moves.
     """
+    poly_len = np.min([polymer.r.shape[0] for polymer in polymers])
     return [
         controller(
-            mv.MCAdapter(str(log_dir) + '/' + move.__name__, move)
+            mv.MCAdapter(
+                str(log_dir) + '/' + move.__name__, move
+            ),
+        bead_amp_bounds=(1, poly_len),
+        move_amp_bounds=move_amp_bounds
         ) for move in [
             mv.crank_shaft, mv.end_pivot, mv.slide, mv.tangent_rotation
         ]

chromo/mc/moves.py
@@ -204,7 +204,12 @@ class MCAdapter:
 
         self.num_success += 1
         self.performance_tracker.update_acceptance_rate(accept=True)
-        self.performance_tracker.log_move(self.last_amp_move, self.last_amp_bead)
+        self.performance_tracker.log_move(
+            self.amp_move,
+            self.amp_bead,
+            self.last_amp_move,
+            self.last_amp_bead
+        )
         self.performance_tracker.log_acceptance_rate()
 
     def reject(self):
@@ -213,7 +218,12 @@ class MCAdapter:
         Log the rejected move in the performance tracker
         """
         self.performance_tracker.update_acceptance_rate(accept=False)
-        self.performance_tracker.log_move(self.last_amp_move, self.last_amp_bead)
+        self.performance_tracker.log_move(
+            self.amp_move,
+            self.amp_bead,
+            self.last_amp_move,
+            self.last_amp_bead
+        )
         self.performance_tracker.log_acceptance_rate()
 
 
@@ -248,7 +258,11 @@ def crank_shaft(polymer: Polymer, amp_move: MOVE_AMP, amp_bead: BEAD_AMP):
         indicator that a continuous segment of beads were affected,
         and the move and bead amplitudes
     """
-    delta_ind = min(np.random.randint(2, amp_bead), polymer.num_beads)
+    delta_ind = np.min(
+        [np.max(
+            [np.random.randint(0, amp_bead), 2]
+        ), polymer.num_beads]
+    )
     ind0 = np.random.randint(polymer.num_beads - delta_ind)
     indf = ind0 + delta_ind
     inds = np.arange(ind0, indf)
@@ -343,15 +357,81 @@ def conduct_crank_shaft(
 
 def end_pivot(polymer: Polymer, amp_move: MOVE_AMP, amp_bead: BEAD_AMP):
     """
-    Randomly rotate segment from one end of the polymer.
+    Randomly rotate segment from end of polymer about random axis.
 
     Stochastic component of end-pivot move. Begin by selecting a random
     rotation angle based on the move amplitude. Then isolate a random selection
     of beads on either the LHS or RHS of the polymer based on the move
     amplitude. Isolate the homogeneous coordinates for the selected beads, and
     format those coordinates for matrix multiplication. Perform the
-    transformation associated with the end pivot move, and reformat the
-    homogeneous coordinates as cartesian coordinates.
+    transformation associated with the end pivot move, with random axis of
+    rotation selected along the unit sphere. Reformat homogeneous coordinates
+    as cartesian coordinates.
+
+    Parameters
+    ----------
+    polymer : Polymer
+        Polymer object
+    amp_move : float
+        Maximum amplitude (rotation angle) of the end-pivot move
+    amp_bead : int
+        Maximum amplitude (number) of beads affected by the end-pivot move
+
+    Returns
+    -------
+    _proposal_arg_types
+        Affected bead indices, their positions and orientations, and an
+        indicator that a continuous segment of beads were affected
+    """
+    rot_angle = amp_move * (np.random.rand() - 0.5)
+
+    num_beads = polymer.num_beads
+    if np.random.randint(0, 2) == 0:    # rotate LHS
+        ind0 = 0
+        indf = beads.select_bead_from_left(
+            amp_bead, num_beads, exclude_last_bead=False
+        ) + 1
+    else:                               # rotate RHS
+        ind0 = beads.select_bead_from_right(
+            amp_bead, num_beads, exclude_first_bead=False
+        )
+        indf = num_beads + 1
+
+    inds = np.arange(ind0, indf)
+    continuous_inds = True
+    r_points = np.ones((4, indf-ind0))
+    r_points[0:3, :] = polymer.r[ind0:indf, :].T
+    r_rot_0 = (0, 0, 0)
+    r_rot_1 = linalg.uniform_sample_unit_sphere()
+    t3_points = np.ones((4, indf-ind0))
+    t3_points[0:3, :] = polymer.t3[ind0:indf, :].T
+    t2_points = np.ones((4, indf-ind0))
+    t2_points[0:3, :] = polymer.t2[ind0:indf, :].T
+
+    r_trial, t3_trial, t2_trial = conduct_end_pivot(
+        r_points, r_rot_0, r_rot_1, t3_points, t2_points, rot_angle
+    )
+    r_trial = r_trial[0:3, :].T
+    t3_trial = t3_trial[0:3, :].T
+    t2_trial = t2_trial[0:3, :].T
+
+    return (inds, r_trial, t3_trial, t2_trial, None, continuous_inds, len(inds),
+        rot_angle)
+
+
+def on_axis_end_pivot(polymer: Polymer, amp_move: MOVE_AMP, amp_bead: BEAD_AMP):
+    """
+    Randomly rotate segment from end of polymer about axis w/ neighboring bead.
+
+    See documentation for `end_pivot` – the only difference comes in selection
+    of the rotation axis. While `end_pivot` selects a random rotation axis on
+    the unit sphere, `on_axis_end_pivot` selects the axis of rotation formed
+    from the vector connecting the inner-most bead affected by the move and its
+    inner neighboring bead on the chain.
+
+    NOTE: This move is currently inactive, but may be added in the future.
+    
+    TODO: If implementing this move, double check bead selection approach.
 
     Parameters
     ----------
@@ -381,24 +461,21 @@ def end_pivot(polymer: Polymer, amp_move: MOVE_AMP, amp_bead: BEAD_AMP):
         indf = num_beads
         pivot_point = ind0
         base_point = ind0 - 1
+
     inds = np.arange(ind0, indf)
     continuous_inds = True
-
     r_points = np.ones((4, indf-ind0))
     r_pivot = polymer.r[pivot_point, :]
     r_base = polymer.r[base_point, :]
     r_points[0:3, :] = polymer.r[ind0:indf, :].T
-
     t3_points = np.ones((4, indf-ind0))
     t3_points[0:3, :] = polymer.t3[ind0:indf, :].T
-
     t2_points = np.ones((4, indf-ind0))
     t2_points[0:3, :] = polymer.t2[ind0:indf, :].T
 
     r_trial, t3_trial, t2_trial = conduct_end_pivot(
         r_points, r_pivot, r_base, t3_points, t2_points, rot_angle
     )
-
     r_trial = r_trial[0:3, :].T
     t3_trial = t3_trial[0:3, :].T
     t2_trial = t2_trial[0:3, :].T
@@ -425,10 +502,10 @@ def conduct_end_pivot(
     ----------
     r_points : array_like (4, N)
         Homogeneous coordinates for beads undergoing rotation
-    r_pivot : array_like (4,)
-        Homogeneous coordinates for beads about which the pivot occurs
-    r_base : array_like (4,)
-        Homogeneous coordinates for bead establishing axis of rotation
+    r0 : array_like (3,)
+        First coordinate of rotation axis
+    r1 : array_like (3,)
+        Second coordinate of rotation axis
     t3_points : array_like (4, N)
         Homogeneous tangent vectors for beads undergoing rotation
     t2_points : array_like (4, N)
@@ -445,7 +522,7 @@ def conduct_end_pivot(
     t2_trial : array_like (4, N)
         Homogeneous tangent vectors, orthogonal to t3_trial, following rotation
     """
-    rot_matrix = linalg.arbitrary_axis_rotation(r_pivot, r_base, rot_angle)
+    rot_matrix = linalg.arbitrary_axis_rotation(r0, r1, rot_angle)
     r_trial = rot_matrix @ r_points
     t3_trial = rot_matrix @ t3_points
     t2_trial = rot_matrix @ t2_points

chromo/util/bead_selection.py
@@ -12,10 +12,16 @@ def capped_exponential(window, cap=np.inf):
     """
     Select exponentially distributed integer below some capped value.
 
+    Specify the geometric distribution such that 99% of outcomes fall below
+    window. Do this by manipulating the argument of `np.random.geometric()`
+    and selecting a success probability such that the CDF of the geometric
+    distribution at `window` equals 0.99. This will significantly improve
+    runtime, particularly at the while loop.
+
     Parameters
     ----------
     window : int
-        Mean of the exponential distribution being sampled
+        99th percentile of the exponential distribution being sampled
     cap : int
         Maximum value of exponentially sampled integer 
     Returns
@@ -23,10 +29,16 @@ def capped_exponential(window, cap=np.inf):
     r : int
         Exponentially sampled random integer less than capped value
     """
+    eCDF_val_at_window = 0.99
+    p = 1 - (1 - eCDF_val_at_window) ** (1 / (window + 1))
 
-    r = np.random.geometric(1/window)
+    try:
+        r = np.random.geometric(p)
+    except:
+        raise ValueError("Selection window " + str(window) + " is less than 1.")
+
     while r > cap:
-        r = np.random.geometric(1/window)
+        r = np.random.geometric(p)
     return r
 
 def select_bead_from_left(window, N_beads, exclude_last_bead=True):
@@ -54,10 +66,10 @@ def select_bead_from_left(window, N_beads, exclude_last_bead=True):
     if window > N_beads:
         raise ValueError("Bead selection window size must be less than polymer length")
     
-    return capped_exponential(window, N_beads)
+    return capped_exponential(window, window)
 
 
-def select_bead_from_right(window, N_beads, exclude_first_bead = True):
+def select_bead_from_right(window, N_beads, exclude_first_bead=True):
     """
     Randomly select index exponentially decaying from right.
 

chromo/util/linalg.py
@@ -1,14 +1,39 @@
-"""Utility functions for linear algebra calculations."""
+"""Utility functions for linear algebra calculations.
+"""
+
+# Built-in Modules
+from typing import Tuple
+from random import uniform
+
+# External Modules
 import numpy as np
 from scipy.spatial.transform import Rotation
+
+
+def uniform_sample_unit_sphere():
+    """Randomly sample a vector on the unit sphere.
+
+    Returns
+    -------
+    Tuple[float, float, float]
+        Random (x, y, z) vector on unit sphere
+    """
+    phi = np.random.uniform() * (2 * np.pi)
+    theta = np.arccos(uniform(-1, 1))
+    return (
+        np.cos(phi) * np.sin(theta),    # x
+        np.sin(phi) * np.sin(theta),    # y
+        np.cos(theta)                   # z
+    )
+
 
 def arbitrary_axis_rotation(r0, r1, rot_angle):
     """
     Rotate about an axis defined by two points.
 
     Generate a transformation matrix for counterclockwise (right handed
-    convention) rotation of angle *rot_angle* about an arbitrary axis from
-    points *r0* to *r1*.
+    convention) rotation of angle `rot_angle` about an arbitrary axis from
+    points `r0` to `r1`.
 
     Parameters
     ----------
@@ -18,7 +43,7 @@ def arbitrary_axis_rotation(r0, r1, rot_angle):
         Second point defining axis to rotate around.
     rot_angle : float
         Angle of rotation. Positive rotation is counterclockwise when the
-        vector `r1 - r0` is pointing directly out of the blackboard.
+        vector `r1 - r0` is pointing directly out of the screen.
 
     Returns
     -------
@@ -71,5 +96,3 @@ def generate_translation_mat(delta_x, delta_y, delta_z):
     translation_mat[2, 3] = delta_z
 
     return translation_mat
-
-